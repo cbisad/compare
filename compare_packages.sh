ce9ac42cc6a0d71ffc9404a83dc45990a714ebe9
Charles Benon <36412699+charlesbenon@users.noreply.github.com>
2020-03-30 15:07:44 +0200

    Update compare_packages.sh

--- compare_packages.sh
+++ compare_packages.sh
@@ -1,88 +1,99 @@
-#Variables
-input_file='/tmp/ip-nodetype.txt'
-stackrc_file='/home/stack/stackrc'
-openstack_user='heat-admin'
-report_file='/tmp/diff-report.txt'
-ssh_commands=" rpm -qa | grep openstack && sudo docker ps --format '{{.Image}}' "
+#Script to compare OpenStack nodes per type
+#a. Run the script from Undercloud/Director as stack user
+#b. Check the variables to make sure they match the environment
 
-#Arrays
-declare IP_NodeType_Array Controller_IP_array ComputeDpdkHw0_IP_array CephStorageHw10_IP_array AppformixController_IP_array ContrailController_IP_array ContrailAnalyticsDatabase_IP_array ContrailAnalytics_IP_array
-Controller_IP_array=( init )
-ComputeDpdkHw0_IP_array=( init )
-CephStorageHw10_IP_array=( init )
-AppformixController_IP_array=( init )
-ContrailController_IP_array=( init )
-ContrailAnalyticsDatabase_IP_array=( init )
-ContrailAnalytics_IP_array=( init )
+#Variables
+dc_path='/tmp/dc/'
+input_file=${dc_path}ip-nodeflavor.txt
+report_file=${dc_path}diff-report.txt
+buffer_file=${dc_path}sort_buffer.txt
+openstack_stackrc='/home/stack/stackrc'
+openstack_user='heat-admin'
+ssh_commands=" rpm -qa | grep -E 'appformix*|ceph*|container*|contrail*|corosync*|docker*|galera*|haproxy*|hiera*|ipa*|kernel*|mariadb*|memcached*|openstack*|openvswitch*|pacemaker*|pcs*|postgresql*|puppet*|python*' & sudo docker ps --format '{{.Image}}' "
+
+#Each type of node needs an array to store its IP and Flavor
+arrays="IP_NodeFlavor_Array Controller_IP_array Compute_0_IP_array Compute_1_IP_array Compute_2_IP_array CephStorage_0_IP_array CephStorage_1_IP_array AppformixController_IP_array ContrailController_IP_array ContrailAnalyticsDatabase_IP_array ContrailAnalytics_IP_array"
+
+#Each type of node needs its OpenStack Flavor
+openstack_controller=Controller
+openstack_compute_0=ComputeDpdkHw0
+openstack_compute_1=ComputeDpdk10Hw0
+openstack_compute_2=ComputeDpdk11Hw0
+openstack_storage_0=CephStorage10Hw5
+openstack_storage_1=CephStorage11Hw5
+contrail_appformix=AppformixController
+contrail_controller=ContrailController
+contrail_analytics_db=ContrailAnalyticsDatabase
+contrail_analytics=ContrailAnalytics
+
+###Arrays declaration and init
+for array in $arrays; do declare $array; eval "$array=( init )"; done
 
 #Reset data files
 cat /dev/null > $report_file
 cat /dev/null > $input_file
 
-#Collect IP and NodeType information from Undercloud and sort nodes per types
-source $stackrc_file
+
+#1.Collect IP and Flavor information from Undercloud and sort nodes per flavor
+echo '1.Collect IP and Flavor information from Undercloud'
+mkdir -p $dc_path
+source $openstack_stackrc
 openstack server list | awk '{print $8, $12}' | awk -F= '{print $2}' | awk '!/^$/' >> $input_file
-sort -k2 $input_file > /tmp/sort_buffer; cp /tmp/sort_buffer $input_file
+sort -k2 $input_file > $buffer_file; cp $buffer_file $input_file
+echo '1.DONE'
 
-#SSH to each node, collect datas, copy them locally in NodeType-IP filename and sort the datas to make the comparison output easier to read
+
+#2.SSH to each node, collect datas, copy them locally in <Flavor>-<IP> filename and sort the datas to make the comparison output easier to read
+echo '2.Collect datas from each node'
 while read file_line; 
-do IP_NodeType_Array=($file_line);
-ssh -l $openstack_user ${IP_NodeType_Array[0]} $ssh_commands > /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} < /dev/null; 
-sort /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} > /tmp/sort_buffer; cp /tmp/sort_buffer /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]};
+do IP_NodeFlavor_Array=($file_line);
+ssh -l $openstack_user ${IP_NodeFlavor_Array[0]} $ssh_commands > $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} < /dev/null; 
+sort $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} > $buffer_file; cp $buffer_file $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]};
 done < $input_file
+#Add director (useful when comparing DCs)
+rpm -qa | sort > ${dc_path}Director-127.0.0.1 && echo "127.0.0.1 Director" >> $input_file
+echo '2.DONE'
 
-#Loop going through the list of IPs/NodeType and comparing the NodeType-IP files
+
+#3.Loop going through the list of IPs/Flavor and comparing the <Flavor>-<IP> files
+echo '3.Comparing datas between node flavors'
 while read file_line; 
-do IP_NodeType_Array=($file_line); declare IP_NodeType_Array;
-echo '*****' Node: ${IP_NodeType_Array[0]} ${IP_NodeType_Array[1]} '*****' >> $report_file
-        # case to split node per type and compare datas with the first node of the list only
-        case ${IP_NodeType_Array[1]} in
-                "Controller") 
-                              # Check if the variable has been used already, if not, will initilize it with the first node IP of the list 
-                              # and this will be used to compare with all the other node IPs in the same category
-                              if [ ${Controller_IP_array[0]} = "init" ]
-                              then
-                                Controller_IP_array=${IP_NodeType_Array[0]}
-                              fi
-                              diff /tmp/${IP_NodeType_Array[1]}-${Controller_IP_array[0]} /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} >> $report_file ;;
-                "ComputeDpdkHw0")
-                              if [ ${ComputeDpdkHw0_IP_array[0]} = "init" ]
-                              then
-                                ComputeDpdkHw0_IP_array=${IP_NodeType_Array[0]}
-                              fi
-                              diff /tmp/${IP_NodeType_Array[1]}-${ComputeDpdkHw0_IP_array[0]} /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} >> $report_file ;;
-                "CephStorageHw10")
-                              if [ ${CephStorageHw10_IP_array[0]} = "init" ]
-                              then
-                                CephStorageHw10_IP_array=${IP_NodeType_Array[0]}
-                              fi
-                              diff /tmp/${IP_NodeType_Array[1]}-${CephStorageHw10_IP_array[0]} /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} >> $report_file ;;
-                "AppformixController")
-                              if [ ${AppformixController_IP_array[0]} = "init" ]
-                              then
-                                AppformixController_IP_array=${IP_NodeType_Array[0]}
-                              fi
-                              diff /tmp/${IP_NodeType_Array[1]}-${AppformixController_IP_array[0]} /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} >> $report_file ;;
-                "ContrailController")
-                              if [ ${ContrailController_IP_array[0]} = "init" ]
-                              then
-                                ContrailController_IP_array=${IP_NodeType_Array[0]}
-                              fi
-                              diff /tmp/${IP_NodeType_Array[1]}-${ContrailController_IP_array[0]} /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} >> $report_file ;;
-                "ContrailAnalyticsDatabase")
-                              if [ ${ContrailAnalyticsDatabase_IP_array[0]} = "init" ]
-                              then
-                                ContrailAnalyticsDatabase_IP_array=${IP_NodeType_Array[0]}
-                              fi
-                              diff /tmp/${IP_NodeType_Array[1]}-${ContrailAnalyticsDatabase_IP_array[0]} /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} >> $report_file ;;
-                "ContrailAnalytics")
-                              if [ ${ContrailAnalytics_IP_array[0]} = "init" ]
-                              then
-                                ContrailAnalytics_IP_array=${IP_NodeType_Array[0]}
-                              fi
-                              diff /tmp/${IP_NodeType_Array[1]}-${ContrailAnalytics_IP_array[0]} /tmp/${IP_NodeType_Array[1]}-${IP_NodeType_Array[0]} >> $report_file ;;
-                *) echo "Invalid condition, exiting"
+do IP_NodeFlavor_Array=($file_line); declare IP_NodeFlavor_Array;
+echo '*****' Node: ${IP_NodeFlavor_Array[0]} ${IP_NodeFlavor_Array[1]} '*****' >> $report_file
+        # case to split node per flavor and compare datas with the first node of the list only (reference node)
+        case ${IP_NodeFlavor_Array[1]} in
+                "$openstack_controller") 
+                              # Check if the variable has been used already, if not, will initilize it with the first node IP of the list to compare against other nodes with the same flavor
+                              if [ ${Controller_IP_array[0]} = "init" ]; then
+                                Controller_IP_array=${IP_NodeFlavor_Array[0]}; fi
+                              diff $dc_path${IP_NodeFlavor_Array[1]}-${Controller_IP_array[0]} $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} >> $report_file ;;
+                "$openstack_compute_0")
+                              if [ ${Compute_0_IP_array[0]} = "init" ]; then
+                                Compute_0_IP_array=${IP_NodeFlavor_Array[0]}; fi
+                              diff $dc_path${IP_NodeFlavor_Array[1]}-${Compute_0_IP_array[0]} $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} >> $report_file ;;
+                "$openstack_storage_0")
+                              if [ ${CephStorage_0_IP_array[0]} = "init" ]; then
+                                CephStorage_0_IP_array=${IP_NodeFlavor_Array[0]}; fi
+                              diff $dc_path${IP_NodeFlavor_Array[1]}-${CephStorage_0_IP_array[0]} $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} >> $report_file ;;
+                "$contrail_appformix")
+                              if [ ${AppformixController_IP_array[0]} = "init" ]; then
+                                AppformixController_IP_array=${IP_NodeFlavor_Array[0]}; fi
+                              diff $dc_path${IP_NodeFlavor_Array[1]}-${AppformixController_IP_array[0]} $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} >> $report_file ;;
+                "$contrail_controller")
+                              if [ ${ContrailController_IP_array[0]} = "init" ]; then
+                                ContrailController_IP_array=${IP_NodeFlavor_Array[0]}; fi
+                              diff $dc_path${IP_NodeFlavor_Array[1]}-${ContrailController_IP_array[0]} $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} >> $report_file ;;
+                "$contrail_analytics_db")
+                              if [ ${ContrailAnalyticsDatabase_IP_array[0]} = "init" ]; then
+                                ContrailAnalyticsDatabase_IP_array=${IP_NodeFlavor_Array[0]}; fi
+                              diff $dc_path${IP_NodeFlavor_Array[1]}-${ContrailAnalyticsDatabase_IP_array[0]} $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} >> $report_file ;;
+                "$contrail_analytics")
+                              if [ ${ContrailAnalytics_IP_array[0]} = "init" ]; then
+                                ContrailAnalytics_IP_array=${IP_NodeFlavor_Array[0]}; fi
+                              diff $dc_path${IP_NodeFlavor_Array[1]}-${ContrailAnalytics_IP_array[0]} $dc_path${IP_NodeFlavor_Array[1]}-${IP_NodeFlavor_Array[0]} >> $report_file ;;
+                *) echo "Node comparison failed, check the node flavors"
                    exit 1 ;;
         esac
 done < $input_file
-echo Report: $report_file
+echo '3.DONE'
+echo "Report can be found here:" $report_file
